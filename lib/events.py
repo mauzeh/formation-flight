c8903fede72e97450b0affa909872d7eb6a11e1a
Maurits Dekkers <bluedackers@gmail.com>
2012-08-07 12:21:45 +0200

    Improved event prints

--- lib/events.py
+++ lib/events.py
@@ -1,4 +1,5 @@
 from pydispatch import dispatcher
+from formation_flight.aircraft import Aircraft
 
 class EventHandler:
     """
@@ -18,9 +19,30 @@ class EventHandler:
         if(signal in self.ignore):
             return 0
 
-        print '+----------------------------------------------+'
-        print '| Time: %s units' % time
+        debug = []
+
+        print '+-----------------------------------------------------+'
         print '| %s :: %s' % (sender.__class__.__name__, signal)
-        print '| Data: %s' % data
-        print '| %s' % sender
-        print '+----------------------------------------------+'
+        print '+-----------------------------------------------------+'
+        debug.append(('| % 25s: %s units', ('Time', time)))
+
+        if type(data) == Aircraft:
+
+            segment = data.route.get_current_segment(data.get_distance_flown())
+            l       = segment.get_length()
+            d       = data.route.get_distance_into_current_segment(data.get_distance_flown())
+            eta     = time + (l - d) / data.speed
+
+            debug.append(('| % 25s: %s', ('Aircraft', data.name)))
+            debug.append(('| % 25s: %.1f', ('Speed', data.speed)))
+            debug.append(('| % 25s: %s (%.1f km)', ('Segment', segment, l)))
+            debug.append(('| % 25s: %.1f km', ('Distance into segment', d)))
+            debug.append(('| % 25s: %.1f', ('Waypoint ETA', eta)))
+
+        else:
+            print '| Data: %s' % data
+
+        for line in debug:
+            format, data = line
+            print format % data
+        print '+-----------------------------------------------------+'
