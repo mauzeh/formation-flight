8542beece52ba367811fdf08e06b9aee4811cf41
Maurits Dekkers <bluedackers@gmail.com>
2012-09-25 17:49:36 +0200

    Added some debug info for formations

--- lib/debug.py
+++ lib/debug.py
@@ -11,7 +11,7 @@ from formation_flight.formation import Formation
 signals = [
     #'fly',
     #'sim-init',
-    #'formation-init',
+    'formation-init',
     'formation-locked',
     #'takeoff',
     'waypoint-reached'
@@ -48,6 +48,7 @@ def handle(signal, sender, data = None, time = 0):
         lines.append('| %25s: %s' % ('Participants', data.aircraft))
         lines.append('| %25s: %.2f' % ('Start ETA', data.get_start_eta()))
         lines.append('| %25s: %s' % ('Status', data.status))
+        lines.append('| %25s: %s' % ('Hub', data.hub))
         pass
 
     else:
