5fcb717fc2420c1226077e5846ac8b2e774c1fbc
Maurits Dekkers <bluedackers@gmail.com>
2012-10-16 14:38:08 +0200

    Intervals having zero length to be grouped. Fixes #5.

--- lib/intervals.py
+++ lib/intervals.py
@@ -5,7 +5,7 @@ class Interval:
     A time window that is used to group flights arriving at about the same time.
     """
     def __init__(self, obj, start, end):
-        assert start < end
+        assert start <= end
         self.obj = obj
         self.start = start
         self.end = end
@@ -23,14 +23,14 @@ def group(intervals):
     solution = []
     candidates = intervals
     for i in range(0, len(candidates)):
-        if candidates[i].start < candidates[0].end:
+        if candidates[i].start <= candidates[0].end:
             solution.append(candidates[i])
     return [solution] + group(list(set(candidates) - set(solution)))
 
 if __name__ == "__main__":
     print group([
         Interval('G', 0, 2),
-        Interval('B', 2, 4),
+        Interval('B', 4, 4),
         Interval('C', 1, 3),
         Interval('D', 3, 5),
         Interval('A', 1, 3),
