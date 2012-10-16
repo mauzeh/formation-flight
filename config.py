1e69c2bf489eb5132322d76246b69d06da942fe9
Maurits Dekkers <bluedackers@gmail.com>
2012-10-16 18:19:08 +0200

    Slack is now a function of the lock time.

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 # events_printed = ['aircraft-depart', 'aircraft-at-waypoint', 'formation-alive', 'aircraft-arrive']
 events_printed = ['formation-alive']#, 'aircraft-arrive']
 
-# How much the arrival at the virtual hub can be delayed/expedited (mins)
-etah_slack = 5
-
 # How long before hub arrival can we allocate into formations? (mins)
-lock_time = 30
+lock_time = 10
+
+# How much the arrival at the virtual hub can be delayed/expedited (mins)
+etah_slack = lock_time / 10
